bcba0cc652e56b490d163a6e5ae5f555e176d87e
Martin Sandström <martin@marteinn.se>
2017-11-12 18:26:13 +0100

    Add py3 classifier

--- setup.py
+++ setup.py
@@ -66,6 +66,7 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 2',
         "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
         'Framework :: Django',
         'Topic :: Utilities',
     ],
