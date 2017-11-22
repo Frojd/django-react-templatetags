87986abe2b3fb8951a571ff0a850f25559201dc2
Martin Sandström <martin@marteinn.se>
2017-11-22 11:22:01 +0100

    PEP8 fixes

--- django_react_templatetags/context_processors.py
+++ django_react_templatetags/context_processors.py
@@ -6,5 +6,5 @@ def react_context_processor(request):
     """Expose a global list of react components to be processed"""
 
     return {
-        'REACT_COMPONENTS': []
+        'REACT_COMPONENTS': [],
     }
