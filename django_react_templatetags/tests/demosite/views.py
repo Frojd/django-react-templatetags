8b73fe5e1d8e44c5dbdc5f68e166de744c0543f7
Martin Sandström <martin@marteinn.se>
2019-04-22 20:16:33 +0200

    More py 3 fixes

--- django_react_templatetags/tests/demosite/views.py
+++ django_react_templatetags/tests/demosite/views.py
@@ -5,7 +5,7 @@ class StaticReactView(TemplateView):
     template_name = "static-react.html"
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(StaticReactView, self).get_context_data(**kwargs)
         context["props"] = {
             "artist": "Tom Waits",
             "recent_album": "Bad as me",
