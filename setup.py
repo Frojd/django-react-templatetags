947c6ce9ee0197a1904ad0e4441dc234dfc92707
Martin Sandström <martin@marteinn.se>
2019-04-24 12:44:57 +0200

    Update pypi classifiers

--- setup.py
+++ setup.py
@@ -63,7 +63,11 @@ setup(
         'Programming Language :: Python :: 2',
         "Programming Language :: Python :: 2.7",
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Django',
         'Topic :: Utilities',
+        'Programming Language :: JavaScript',
     ],
 )
