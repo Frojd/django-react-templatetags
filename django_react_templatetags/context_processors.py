54296c607b735ce06b3420efecb312f52876e012
Martin Sandström <martin@marteinn.se>
2020-01-28 17:06:31 +0100

    Replace warning message with deprecation warning

--- django_react_templatetags/context_processors.py
+++ django_react_templatetags/context_processors.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import warnings
+
 
 def react_context_processor(request):
     """Expose a global list of react components to be processed"""
-    print("react_context_processor is no longer required.")
+    warnings.warn(
+        "react_context_processor is no longer required.", DeprecationWarning
+    )
 
     return {
         'REACT_COMPONENTS': [],
