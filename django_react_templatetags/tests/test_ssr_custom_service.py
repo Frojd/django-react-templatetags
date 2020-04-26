bd23b85b961b66c858a4fd3e4d67f32e056ed6cb
Martin Sandström <martin@marteinn.se>
2020-04-26 15:20:11 +0200

    Refactor tests #49

---
+++ django_react_templatetags/tests/test_ssr_custom_service.py
@@ -0,0 +1,26 @@
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from django.urls import reverse
+from django.test import TestCase, override_settings
+
+
+@override_settings(
+    REACT_RENDER_HOST='http://react-service.dev/',
+    REACT_SSR_SERVICE="django_react_templatetags.tests.test_ssr_custom_service.CustomSSRService",
+)
+class CustomSSRServiceTest(TestCase):
+    @mock.patch("django_react_templatetags.tests.test_ssr_custom_service.CustomSSRService.load_or_empty")
+    def test_that_disable_ssr_header_disables_ssr(self, mocked_func):
+        self.client.get(
+            reverse('static_react_view'),
+        )
+        self.assertEqual(mocked_func.call_count, 1)
+
+
+class CustomSSRService():
+    "Used for testing using a custom ssr service"
+    def load_or_empty(self, component, headers={}, ssr_context=None):
+        pass
