74b7e9ef4ab947abb3fe2e5e4fc2af091b4e4520
Martin Sandström <martin@marteinn.se>
2020-09-05 15:59:45 +0200

    Drop warning and enforce middleware requirement again. Fixes #54

--- django_react_templatetags/context_processors.py
+++ django_react_templatetags/context_processors.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import warnings
-
-
 def react_context_processor(request):
     """Expose a global list of react components to be processed"""
-    warnings.warn(
-        "react_context_processor is no longer required.", DeprecationWarning
-    )
 
     return {
         'REACT_COMPONENTS': [],
