c0b453cb45fd6432afc3584abe8310d410bcc46c
Martin Sandström <martin@marteinn.se>
2022-01-04 08:28:27 +0100

    Clarify that secret key is just an example

--- example_django_react_templatetags/examplesite/settings.py
+++ example_django_react_templatetags/examplesite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "6l8*qp!k0g^^y4rglr3@ny-&41%y=k4d0o+t!!=uk4!h)aot73"
+SECRET_KEY = "EXAMPLE_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
