6f6dcde75a8b521dceb0cd64dfae4b320d3e17ae
Martin Sandström <martin@marteinn.se>
2023-01-07 09:05:13 +0100

    Bump version to 7.0.1

--- django_react_templatetags/__init__.py
+++ django_react_templatetags/__init__.py
@@ -7,8 +7,8 @@ This extension allows you to add React components into your django templates.
 """
 
 __title__ = "django_react_templatetags"
-__version__ = "7.0.0"
-__build__ = 700
+__version__ = "7.0.1"
+__build__ = 701
 __author__ = "Martin Sandström"
 __license__ = "MIT"
-__copyright__ = "Copyright 2015-2021 Fröjd Interactive"
+__copyright__ = "Copyright 2015-2022 Fröjd Interactive"
