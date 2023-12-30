4867a9d852a80d3581d95ec4df8984ec8d3a3795
Martin Sandström <martin@marteinn.se>
2023-12-30 11:05:14 +0100

    Bump license year

--- django_react_templatetags/__init__.py
+++ django_react_templatetags/__init__.py
@@ -11,4 +11,4 @@ __version__ = "7.0.1"
 __build__ = 701
 __author__ = "Martin Sandström"
 __license__ = "MIT"
-__copyright__ = "Copyright 2015-2022 Fröjd Interactive"
+__copyright__ = "Copyright 2015-2024 Fröjd Interactive"
